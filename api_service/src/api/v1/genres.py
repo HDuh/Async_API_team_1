42b791d902c50eb65b2454140bbcefc421186bfd
Nikita Dushkov <gespi@bk.ru>
2022-07-02 16:40:23 +0300

    Добавлен единый "пагинатор"

--- api_service/src/api/v1/genres.py
+++ api_service/src/api/v1/genres.py
@@ -2,24 +2,21 @@ import uuid
 from http import HTTPStatus
 
 from elasticsearch import AsyncElasticsearch
-from fastapi import APIRouter, HTTPException, Query, Depends
+from fastapi import APIRouter, HTTPException, Depends
 from fastapi_cache.decorator import cache
 
 from core import CACHE_EXPIRE_IN_SECONDS
 from db.elastic import get_elastic
 from models import Genre
-from .schemas import GenreApiSchema
+from .schemas import GenreApiSchema, Pagination
 
 router = APIRouter()
 
 
-@router.get('',
-            response_model=list[Genre],
-            summary='Get list of films')
+@router.get('', response_model=list[Genre], summary='Get list of films')
 @cache(expire=CACHE_EXPIRE_IN_SECONDS)
 async def all_genres(elastic: AsyncElasticsearch = Depends(get_elastic),
-                     page: int | None = Query(default=1, alias='page[number]', gt=0),
-                     size: int | None = Query(default=50, alias='page[size]', gt=0)) -> list[Genre]:
+                     pagination: Pagination = Depends()) -> list[Genre]:
     """
     ## Get list of genres with the information below:
     - _id_
@@ -27,18 +24,16 @@ async def all_genres(elastic: AsyncElasticsearch = Depends(get_elastic),
 
 
     Supporting query params:
-    - **page[number]** - page number,
-    - **page[size]** - number of items per page
+    - **page_number** - page number,
+    - **page_size** - number of items per page
     """
-    genres = await Genre.manager.filter(elastic, page=page, size=size)
+    genres = await Genre.manager.filter(elastic, **pagination.dict())
     if not genres:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='genres not found')
     return [Genre(**genre.dict()) for genre in genres]
 
 
-@router.get('/{genre_id}',
-            response_model=GenreApiSchema,
-            summary='Info about Genre by ID')
+@router.get('/{genre_id}', response_model=GenreApiSchema, summary='Info about Genre by ID')
 @cache(expire=CACHE_EXPIRE_IN_SECONDS)
 async def detailed_genre_info(genre_id: uuid.UUID,
                               elastic: AsyncElasticsearch = Depends(get_elastic)) -> GenreApiSchema:
