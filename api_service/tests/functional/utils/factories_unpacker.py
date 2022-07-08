bbc351206af6c6011efbfe9e101c95c0f46a9194
sergey.aleshin <serega_aleshin@mail.ru>
2022-07-08 22:39:32 +0300

    теперь используется метод async_build_batch

--- api_service/tests/functional/utils/factories_unpacker.py
+++ api_service/tests/functional/utils/factories_unpacker.py
@@ -1,11 +1,12 @@
 import asyncio
+from typing import List
 
 
-def unpack_factory(factory, size):
-    """"""
+def unpack_factory(factory, size) -> List[dict]:
+    """Распаковывает subfactory в нужную структуру"""
     loop = asyncio.new_event_loop()
     size_int = size.fuzz()
     return [
         model_factory.__dict__
-        for model_factory in loop.run_until_complete(factory.get_factory().async_create_batch(size_int))
+        for model_factory in loop.run_until_complete(factory.get_factory().async_build_batch(size_int))
     ]
