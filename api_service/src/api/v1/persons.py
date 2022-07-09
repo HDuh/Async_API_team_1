d43436ddb6cf5943758b93cb4446fd13b7b306f1
sergey.aleshin <serega_aleshin@mail.ru>
2022-07-10 01:09:34 +0300

    Скорректированы ручки персон

--- api_service/src/api/v1/persons.py
+++ api_service/src/api/v1/persons.py
@@ -1,22 +1,20 @@
 import uuid
 from http import HTTPStatus
+from typing import Any
 
-from elasticsearch import AsyncElasticsearch
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi_cache.decorator import cache
 
 from core import CACHE_EXPIRE_IN_SECONDS
-from db.elastic import get_elastic
 from models import Person, Film
 from .schemas import PersonApiSchema, PersonFilmApiSchema, Pagination
 
 router = APIRouter()
 
 
-@router.get('', response_model=list[PersonApiSchema], summary='Get list of persons')
+@router.get('/', response_model=Any, summary='Get list of persons')
 @cache(expire=CACHE_EXPIRE_IN_SECONDS)
-async def all_persons(elastic: AsyncElasticsearch = Depends(get_elastic),
-                      pagination: Pagination = Depends()) -> list[PersonApiSchema]:
+async def all_persons(pagination: Pagination = Depends()) -> list[PersonApiSchema]:
     """
     ## Get list of genres with the information below:
     - _id_
@@ -29,16 +27,15 @@ async def all_persons(elastic: AsyncElasticsearch = Depends(get_elastic),
     - **page_number_** - page number,
     - **page_size_** - number of items per page
     """
-    persons = await Person.manager.filter(elastic, **pagination.dict())
+    persons = await Person.manager.filter(**pagination.dict())
     if not persons:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='persons not found')
-    return [PersonApiSchema(**person.dict()) for person in persons]
+    return [PersonApiSchema.build_from_model(person) for person in persons]
 
 
-@router.get('/search', response_model=list[PersonApiSchema], summary='Search in persons')
+@router.get('/search', response_model=Any, summary='Search in persons')
 @cache(expire=CACHE_EXPIRE_IN_SECONDS)
-async def search_in_persons(elastic: AsyncElasticsearch = Depends(get_elastic),
-                            query: str | None = None,
+async def search_in_persons(query: str | None = None,
                             pagination: Pagination = Depends()) -> list[PersonApiSchema]:
     """
     ## Get list of persons with the information below:
@@ -53,16 +50,15 @@ async def search_in_persons(elastic: AsyncElasticsearch = Depends(get_elastic),
     - **page_size** - number of items per page
     - **query** - search request
     """
-    persons = await Person.manager.filter(elastic, query=query, **pagination.dict())
+    persons = await Person.manager.filter(query=query, **pagination.dict())
     if not persons:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='persons not found')
-    return [PersonApiSchema(**person.dict()) for person in persons]
+    return [PersonApiSchema.build_from_model(person) for person in persons]
 
 
-@router.get('/{person_id}', response_model=PersonApiSchema, summary='Info about Person by ID')
+@router.get('/{person_id}', response_model=Any, summary='Info about Person by ID')
 @cache(expire=CACHE_EXPIRE_IN_SECONDS)
-async def detailed_person_info(person_id: uuid.UUID,
-                               elastic: AsyncElasticsearch = Depends(get_elastic)) -> PersonApiSchema:
+async def detailed_person_info(person_id: uuid.UUID) -> PersonApiSchema:
     """
     ## Get information about person by ID the information below:
     - _id_
@@ -73,18 +69,15 @@ async def detailed_person_info(person_id: uuid.UUID,
     URL params:
     - **{person_id}**
     """
-    person = await Person.manager.get(elastic, person_id)
+    person = await Person.manager.get(person_id)
     if not person:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='person not found')
-    return PersonApiSchema(**person.dict())
+    return PersonApiSchema.build_from_model(person)
 
 
-@router.get('/{person_id}/film',
-            response_model=list[PersonFilmApiSchema],
-            summary='List of films for a specified person by id')
+@router.get('/{person_id}/film', response_model=Any, summary='List of films for a specified person by id')
 @cache(expire=CACHE_EXPIRE_IN_SECONDS)
-async def person_films(person_id: uuid.UUID,
-                       elastic: AsyncElasticsearch = Depends(get_elastic)) -> list[PersonFilmApiSchema]:
+async def person_films(person_id: uuid.UUID) -> list[PersonFilmApiSchema]:
     """
     ## Get list of films by Person ID with the information below:
     - _id_
@@ -95,7 +88,7 @@ async def person_films(person_id: uuid.UUID,
     URL params:
     - **{person_id}**
     """
-    all_films = await Film.manager.filter(elastic, person=person_id)
+    all_films = await Film.manager.filter(person=person_id)
     if not all_films:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='films not found')
-    return [PersonFilmApiSchema(**film.dict()) for film in all_films]
+    return [PersonFilmApiSchema.build_from_model(film) for film in all_films]
