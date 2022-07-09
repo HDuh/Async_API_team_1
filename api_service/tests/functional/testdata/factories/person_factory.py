305e07f42776d8984718a53ee22d7ec06fc9a304
sergey.aleshin <serega_aleshin@mail.ru>
2022-07-09 23:16:23 +0300

    Добавлен enum c ролями

--- api_service/tests/functional/testdata/factories/person_factory.py
+++ api_service/tests/functional/testdata/factories/person_factory.py
@@ -1,6 +1,6 @@
 import factory
 from factory import fuzzy
-
+from functional.utils import RoleTypes
 from src.models import models
 from .base_factory import ElasticBaseFactory
 
@@ -12,7 +12,7 @@ __all__ = (
 class PersonFactory(ElasticBaseFactory):
     id = factory.Faker('uuid4')
     full_name = factory.Faker('name')
-    role = fuzzy.FuzzyChoice(('actor', 'writer', 'director'))
+    role = fuzzy.FuzzyChoice((role.value for role in RoleTypes))
     film_ids = factory.Faker('uuid4')
 
     class Meta:
