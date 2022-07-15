bd989b6ced08d412f664d7ae1e4d3a4ac3d4e557
Nikita Dushkov <gespi@bk.ru>
2022-07-15 12:54:27 +0300

    Добавлено:  - дополнительные кейсы на пагинацию для жанров
    Изменено:
     - фабрика жанров

--- api_service/tests/functional/src/test_genres.py
+++ api_service/tests/functional/src/test_genres.py
@@ -30,7 +30,7 @@ async def test_all_genres(create_list_genres, fastapi_client, redis):
 
 
 async def test_all_genres_pagination_size(create_list_genres, fastapi_client):
-    """Тест правильность размера пагинаци"""
+    """Тест на правильность размера пагинаци"""
     _ = create_list_genres
     random_size = random.randint(1, 10)
 
@@ -40,72 +40,61 @@ async def test_all_genres_pagination_size(create_list_genres, fastapi_client):
     assert random_size == len(response.json())
 
 
-async def test_all_genres_pagination_page(create_list_genres, fastapi_client, es_client):
-    """Тест правильность размера пагинаци"""
+async def test_all_genres_pagination_page(create_list_genres, fastapi_client):
+    """Тест на правильность данных на страничке"""
     genres = create_list_genres
     expected_structure = [
         uuid_to_str(GenreApiSchema.build_from_model(genre)).__dict__
         for genre in genres
     ]
-    page = random.randint(1, 5)
+    page = 4
     page_size = 2
     from_ = 0 if page == 1 else (page - 1) * page_size
     page_structure = expected_structure[from_:from_+page_size]
 
-    # res = await es_client.search(index='genres_test')
-    # for i in res['hits']['hits']:
-    #     print(i['_source']['id'], i['_source']['name'])
-
     response = await fastapi_client.get(f"/api_service/v1/genres/?page_page={page}&page_size={page_size}")
-    full_response = await fastapi_client.get(f"/api_service/v1/genres/")
-    for i in full_response.json():
-        print(i)
-    print('++++++'*15)
-    for i in expected_structure:
-        print(i)
-    print('==='*20)
 
     assert response.status_code == HTTPStatus.OK
     assert page_structure == response.json()
 
 
 async def test_all_genres_pagination_zero_size(create_list_genres, fastapi_client):
-    """Тест правильность размера пагинаци"""
+    """Тест на нулевой размер страницы"""
     _ = create_list_genres
     response = await fastapi_client.get("/api_service/v1/genres/?page_page=1&page_size=0")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
 async def test_all_genres_pagination_zero_page(create_list_genres, fastapi_client):
-    """Тест правильность размера пагинаци"""
+    """Тест на нулевую страницу"""
     _ = create_list_genres
     response = await fastapi_client.get("/api_service/v1/genres/?page_page=0&page_size=5")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
 async def test_all_genres_pagination_inf_page(create_list_genres, fastapi_client):
-    """Тест правильность размера пагинаци"""
+    """Тест на бесконечную страницу"""
     _ = create_list_genres
     response = await fastapi_client.get(f"/api_service/v1/genres/?page_page={float('inf')}&page_size=5")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
 async def test_all_genres_pagination_inf_size(create_list_genres, fastapi_client):
-    """Тест правильность размера пагинаци"""
+    """Тест на бесконечный размер страницы"""
     _ = create_list_genres
     response = await fastapi_client.get(f"/api_service/v1/genres/?page_page=2&page_size={float('inf')}")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
 async def test_all_genres_pagination_incorrect_size(create_list_genres, fastapi_client):
-    """Тест правильность размера пагинаци"""
+    """Тест на не корректное значение размера страницы"""
     _ = create_list_genres
     response = await fastapi_client.get("/api_service/v1/genres/?page_page=2&page_size=some_sting")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
 async def test_all_genres_pagination_incorrect_page(create_list_genres, fastapi_client):
-    """Тест правильность размера пагинаци"""
+    """Тест на не корректное значение страницы"""
     _ = create_list_genres
     response = await fastapi_client.get("/api_service/v1/genres/?page_page=some_string&page_size=4")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
